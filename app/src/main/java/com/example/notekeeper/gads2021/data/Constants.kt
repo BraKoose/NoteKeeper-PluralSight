8f440ced5bf5956f9bc183c8a325b243f567fba3
Koose <KooseTheBlogger@gmail.com>
2022-02-06 22:21:04 +0000

    new changes and files

--- app/src/main/java/com/example/notekeeper/gads2021/data/Constants.kt
+++ app/src/main/java/com/example/notekeeper/gads2021/data/Constants.kt
@@ -1,6 +1,4 @@
 package com.example.notekeeper.gads2021.data
 
-const val NOTE_POSITION = "EXTRA_NOTE_POSITION"
+const val NOTE_POSITION = "NOTE_POSITION"
 const val POSITION_NOT_SET = -1
-const val NOTIFICATION_TAG = "Reminder"
-const val REMINDER_CHANNEL = "reminders"
---
+++ app/src/main/java/com/example/notekeeper/gads2021/data/NoteKeeperData.kt
@@ -0,0 +1,10 @@
+package com.example.notekeeper.gads2021.data
+
+data class CourseInfo (val courseId: String, val title: String) {
+    override fun toString(): String {
+        return title
+    }
+}
+
+data class NoteInfo(var course: CourseInfo? = null, var title: String? = null, var text: String? = null)
+
